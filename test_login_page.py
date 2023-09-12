e61a0cfe884a222ef62df3cd98ec667c50481f5c
nik_mar <marik7581994@gmail.com>
2023-09-12 14:05:58 +0300

    Added new files

---
+++ test_login_page.py
@@ -0,0 +1,22 @@
+import time
+from .pages.login_page import LoginPage
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    time.sleep(3)
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.should_be_login_page()         # выполняем метод страницы — переходим на страницу логина
+    
+    
+#def test_guest_should_see_login_link(browser):
+#    link = "http://selenium1py.pythonanywhere.com/"
+#    page = MainPage(browser, link)
+#    page.open()
+#    page.should_be_login_link()
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,3 +17,5 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
